27167954a070e260b26452dec1d5d5e64c068b6f
uejo <rentarou1994@gmail.com>
2025-05-10 20:31:14 +0900

    :bug: エラーが出るのでいったんコメントしておく

--- Sources/UejobsBot/Utils/OmikujiUtil.swift
+++ Sources/UejobsBot/Utils/OmikujiUtil.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct OmikujiUtil {
-    static func lineMessage(from event: LineEvent) throws -> LineMessage {
-        throw NSError()
-    }
+//    static func lineMessage(from event: LineEvent) throws -> LineMessage {
+//        throw NSError()
+//    }
 }
